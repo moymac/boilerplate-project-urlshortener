e8c7e7629df7a868cf2953071fb0f37ec08bea4a
Moises Macias <lameraweb@hotmail.com>
2024-05-19 19:27:21 +0000

    Finished

--- index.js
+++ index.js
@@ -32,18 +32,31 @@ app.get('/api/hello', function (req, res) {
 
 app.post('/api/shorturl', function (req, res) {
   const url = req.body.url;
-  dns.lookup(url, (error, address) => {
-    if (error) {
-      res.json({ error: 'invalid URL' });
-    } else {
-      const newURL = new URLAddress({ original_url: url });
-      newURL.save(function (err) {
-        if (err) return console.error(err);
-        res.json({ original_url: url, short_url: newURL._id });
-      });
-      // res.json({ original_url: url, short_url: id });
-    }
-  });
+
+  var regex = /^https?:\/\//; //need this format for res.redirect
+
+  if (regex.test(url)) {
+    var tempDnsUrl = url.slice(url.indexOf("//") + 2); //need to remove http(s):// to pass to dns.lookup
+    var slashIndex = tempDnsUrl.indexOf("/"); //need to remove anythng past .com, etc., for dns.lookup
+    var dnsUrl = slashIndex < 0 ? tempDnsUrl : tempDnsUrl.slice(0, slashIndex);
+    console.log("slashIndex: " + slashIndex);
+
+
+    dns.lookup(dnsUrl, (error, address) => {
+      if (error) {
+        res.json({ error: 'invalid URL' });
+      } else {
+        const newURL = new URLAddress({ original_url: url });
+        newURL.save(function (err) {
+          if (err) return console.error(err);
+          res.send({ original_url: url, short_url: newURL._id });
+        });
+        // res.json({ original_url: url, short_url: id });
+      }
+    });
+  } else {
+    res.json({ error: 'invalid URL' });
+  }
 });
 
 app.get('/api/shorturl/:id', function (req, res) {
